2d7f94d1e2d727b4af6c2a3c81015a0265079c9d
ranjan.bisht <ranjan.bisht@tothenew.com>
2017-03-15 18:07:51 +0530

    readme file and pagination implemented

---
+++ README.md
@@ -0,0 +1,32 @@
+********** Basic Steps for Project Setup ***************
+
+1. Clone project from  gitlab to your system with ssh or http
+ repository url. 
+
+        git clone https://github.com/bishtranjan1/trailApp.git
+
+2. Take a pull from master branch.
+
+3. Make sure node is installed in your system.
+
+4. Make sure gulp is installed in your project.
+
+5. Intall Node Modules with command "npm install".
+
+6. Install bower modules with command "bower install"
+
+7. Run your build with gulp command "gulp serve".
+
+
+
+********** Some project  info ***************
+
+
+1- in home page navbar two tabs are ther list and activity
+
+2 - in List tab List of all data is fetch from url and clicking on show details will give  to further detail about that activity
+with map.
+
+3- in Activities tab we can see image and discription of all all activities with images and show detail will show further details with map
+
+
--- src/app/list/list.controller.js
+++ src/app/list/list.controller.js
@@ -9,26 +9,54 @@
     function ListController(ListService, $state) {
         var vm = this;
         var listApi = listApi;
+        vm.placesList = "";
+        vm.listLength = "";
+        vm.currentPage = 1;
+        vm.pageLimit = 25;
+        vm.pageIndex = 0;
+        vm.placesListChunk = [];
+        vm.pageLimitDropDownList = [25, 50, 100];
 
         vm.init = function() {
             listApi();
+
+
+
         }
+
+
         vm.showAction = function(uid) {
             $state.go('main.detail', { uID: uid });
+        }
+        vm.pageLimitListChange = function(limit) {
+            vm.currentPage = 1;
+            vm.pageIndex = 0;
+            vm.pageLimit = parseInt(limit);
+            console.log("pageLimit",vm.pageLimit)
+            generate();
+        };
+        vm.initActivityList = function() {
+            vm.pageIndex = vm.currentPage-1;
+            generate();
+        }
+
+        function generate() {
+
+            var start = vm.pageIndex * vm.pageLimit;
+            var end = start + vm.pageLimit;
+            console.log(start,end)
+            vm.placesListChunk=[];
+            vm.placesListChunk = vm.placesList.slice(start, end);
         }
 
         function listApi() {
             var success = function(resp) {
                 vm.placesList = resp.places;
                 vm.listLength = vm.placesList.length;
+                generate();
             }
             var error = function(err) {}
             ListService.get().$promise.then(success, error);
         }
-
-
-
-
-
     }
 })();
